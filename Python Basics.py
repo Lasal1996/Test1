beaa600e447ffb56d80f490710ee74b9630a4882
Lasal1996 <lasal1996@gmail.com>
2022-07-07 21:40:12 +0530

    Python Basiscs

---
+++ Python Basics.py
@@ -0,0 +1,73 @@
+# # print("Yelow")
+# # age = 20
+# # first_name = "Mosh"
+# # is_online = True
+# # print(is_online)
+
+# # Name = input("Input name: ")
+# # print("Hello "+Name) #String congtenation
+
+# Year = input("Enter the year ")
+# age = 2022 - int(Year)
+# print(age)
+
+# num1 = float(input("1:" ))
+# num2 = input("2:")
+# print ("Answer is " +str((num1) + float(num2)))
+
+# print(5/2) 
+# print(5//2)
+
+# x=45
+# x += 5
+# print(x)
+
+# x = 3>5
+# print(x)  #Answer is false
+
+# if 50 == 5:
+#     print(not(3>7 or 3>1))
+# elif 50 > 55:
+#     print("Yeppy")
+# else:
+#     print("Done")
+
+
+
+# W = input("Enter weight")
+# KK = input("Is it in K or L") 
+
+# if KK.upper() == "K":
+#     print("Weight is " +W)
+# elif KK == "L":
+#     print("Weight is in Lb: "+W)
+# else:
+#     print("Wrong input")
+
+# i=1
+# while(i<5):
+#     print(i * "*")
+#     i += 1
+
+# Name = ["One", "Two" , "Three"]
+# for item in Name:
+#     print(item)
+# Name[0] = "Eka"
+# print(Name[0:2])
+
+# Name.append("four")
+# Name.insert(0,"Yellow")
+# print(2 in Name)
+# print(len(Name))
+"""
+
+num = range(0,50,3)
+for i in num:
+    print(i)
+
+tuple1 = (1,2,3,4) #cant change value
+
+n = int(input())
+k = range(1,n+1)
+for i in k:
+    print(i,end="") #remove spaces between the answers """
